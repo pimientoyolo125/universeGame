fa58c8a13a99e6d4e17aa256dd60cdcddbab73ba
miguel pimiento <mgefrad@gmail.com>
2024-10-03 15:08:22 -0500

    fix(database): update data base scripts

--- ScriptsSQL/TipoProducto.sql
+++ ScriptsSQL/TipoProducto.sql
@@ -1,4 +1,4 @@
-create table tipo_producto (
+create table tipoproducto (
   id bigint primary key generated always as identity,
   nombre text not null,
   descripcion text not null
--- ScriptsSQL/Venta.sql
+++ ScriptsSQL/Venta.sql
@@ -2,9 +2,7 @@ create table ventas (
   id bigint primary key generated always as identity,
   fecha timestamp with time zone not null,
   observaciones text,
-  correo_usuario text not null,
-  id_metodo_pago bigint not null,
+  idusuario bigint not null,
   total double precision not null,
-  constraint fk_correo_usuario_venta foreign key (correo_usuario) references usuarios (correo),
-  constraint fk_metodo_pago foreign key (id_metodo_pago) references metodos_pago (id)
+  constraint fk_correo_usuario_venta foreign key (idusuario) references usuarios (id)
 );
