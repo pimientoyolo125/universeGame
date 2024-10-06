893c0a1161aaa6a6b2c8ba37110081ac2e445567
miguel pimiento <mgefrad@gmail.com>
2024-10-05 21:58:16 -0500

    feat(controllers): add controlles for any table

---
+++ backend/src/main/java/universeGame/backend/controller/DetalleCarritoController.java
@@ -0,0 +1,9 @@
+package universeGame.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/detalle-carrito")
+public class DetalleCarritoController {
+}
