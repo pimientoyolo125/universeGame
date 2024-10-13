ffbb2b07018c3c3ac27b533589c8cd78aca7f66a
miguel pimiento <mgefrad@gmail.com>
2024-10-13 00:11:20 -0500

    feat(productos list): created enpoint for list and find by id product

--- backend/src/main/java/universeGame/backend/controller/UsuarioController.java
+++ backend/src/main/java/universeGame/backend/controller/UsuarioController.java
@@ -1,15 +1,12 @@
 package universeGame.backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuario")
+@Tag(name = "Usuario", description = "Endpoints de usuarios")
 public class UsuarioController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "Test";
-    }
 }
--- backend/src/main/java/universeGame/backend/dto/DetalleCarritoDTO.java
+++ backend/src/main/java/universeGame/backend/dto/DetalleCarritoDTO.java
@@ -1,14 +1,8 @@
 package universeGame.backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class DetalleCarritoDTO {
     private Long id;
     private Long idCarrito;
