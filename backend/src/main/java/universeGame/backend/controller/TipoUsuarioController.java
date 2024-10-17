baf1a1591745c9f3b15e585cb60c766bdc464ddb
miguel pimiento <mgefrad@gmail.com>
2024-10-17 10:35:38 -0500

    feat(tipoProducto): get list types of products

--- backend/src/main/java/universeGame/backend/controller/TipoUsuarioController.java
+++ backend/src/main/java/universeGame/backend/controller/TipoUsuarioController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/tipoUsuario")
+@RequestMapping("/tipo-usuario")
 public class TipoUsuarioController {
 }
