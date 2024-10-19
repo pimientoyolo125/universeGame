bdac4a4ade7ce3c129b7fbf33757b31c77f31034
roa <roaporrasjuandiego@gmail.com>
2024-10-19 00:20:28 -0500

    Adding the Style and the Models for the Sign In and SignUp

--- frontend/src/app/pages/login/signin/signin.component.ts
+++ frontend/src/app/pages/login/signin/signin.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { NgClass } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, CommonModule, FormsModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
-  isPasswordVisible: boolean = false; // Estado para mostrar/ocultar la contraseña
+  isPasswordVisible: boolean = false; 
 
   togglePasswordVisibility() {
-    this.isPasswordVisible = !this.isPasswordVisible; // Cambiar el estado
+    this.isPasswordVisible = !this.isPasswordVisible; 
+  }
+  
+  logPassword: string = '';
+  logEmail: string = '';
+
+  login(): void {
+    console.log("Email: ", this.logEmail, " and ", "Password: ", this.logPassword);
   }
 }
