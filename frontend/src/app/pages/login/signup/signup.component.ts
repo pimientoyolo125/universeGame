9c4d8f2a9fe903d219450245e0a96930308f1128
AndresFelipeMuñozAguilar <munozaguilarandresfelipe@gmail.com>
2024-10-21 10:32:18 -0500

    añadir funcionalidad de feedback en error cuando longitud password <= 8 para sign up

--- frontend/src/app/pages/login/signup/signup.component.ts
+++ frontend/src/app/pages/login/signup/signup.component.ts
@@ -11,10 +11,10 @@ import { CommonModule } from '@angular/common';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
-  isPasswordVisible: boolean = false; 
+  isPasswordVisible: boolean = false;
 
   togglePasswordVisibility() {
-    this.isPasswordVisible = !this.isPasswordVisible; 
+    this.isPasswordVisible = !this.isPasswordVisible;
   }
 
   regName: string = '';
@@ -23,11 +23,44 @@ export class SignupComponent {
   regPassword: string = '';
   regRepPassword: string = '';
   regCheck: boolean = false;
+  isPasswordFocused: boolean = false;
+  isPasswordFocused2: boolean = false;
+
 
   signup(): void {
     console.log("Email: ", this.regEmail, " and ", "Password: ", this.regPassword);
     console.log("Name: ", this.regName, " and ", "LastName: ", this.regLastName);
     console.log("Policies: ", this.regCheck, " and ", "RepeatedPassword:", this.regRepPassword);
   }
+
+
+  // Esta funcion, cada vez que el usuario cambie algo 
+  // en el input de  password, entonces, cambia dinamicamente
+  // el valor de la contraseña
+  assignPasswordValue(event: any) {
+    this.regPassword = event.target.value;
+    // console.log(this.logPassword);
+  }
+
+  assignPasswordValue2(event: any) {
+    this.regRepPassword = event.target.value;
+    // console.log(this.logPassword);
+  }
+
+  // Esta funcion lo que hace es poner en true
+  // una variable, cuando, el input field de la contraseña esté
+  // focusado (se hizo click sobre él). Esto ayuda a manejar la 
+  // visibilidad del <p> que se usa para informar sobre la longitud
+  // que debe tener la contraseña
+  onPasswordFocus() {
+    this.isPasswordFocused = true;
+    // console.log("esta focused input contraseña")
+  }
+
+  onPasswordFocus2() {
+    this.isPasswordFocused2 = true;
+    // console.log("esta focused input contraseña")
+  }
+
 
 }
