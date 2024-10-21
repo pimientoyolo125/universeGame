5146f4cf2fb47c5cf117ccb457fbc30165c70af3
AndresFelipeMuñozAguilar <munozaguilarandresfelipe@gmail.com>
2024-10-21 11:32:50 -0500

    añadir funcion para hacer trim al nombre y el apellido y al phone number

--- frontend/src/app/pages/login/signup/signup.component.ts
+++ frontend/src/app/pages/login/signup/signup.component.ts
@@ -19,6 +19,8 @@ export class SignupComponent {
 
   regName: string = '';
   regLastName: string = '';
+  auxPhoneNumber: string = '';
+  regPhoneNumber: number = 0;
   regEmail: string = '';
   regPassword: string = '';
   regRepPassword: string = '';
@@ -27,9 +29,23 @@ export class SignupComponent {
   isPasswordFocused2: boolean = false;
 
 
+  // aqui se va a hacer el tratamiento de los datos, 
+  // para luego ser enviados al signup
   signup(): void {
+    this.regName = this.getTextBeforeFirstSpace(this.regName);
+    this.regLastName = this.getTextBeforeFirstSpace(this.regLastName);
+
+    // Aquí lo que se hace es primero tomar 
+    // la variable auxiliar para el número de 
+    // teléfono Eliminar sus espacios luego 
+    // de que se eliminara sus espacios 
+    // se convierte de tipo string a tipo 
+    // number
+    this.regPhoneNumber = Number( this.auxPhoneNumber.replace(/\s+/g, '') );
+
     console.log("Email: ", this.regEmail, " and ", "Password: ", this.regPassword);
     console.log("Name: ", this.regName, " and ", "LastName: ", this.regLastName);
+    console.log("Numero de telefono: ", this.regPhoneNumber);
     console.log("Policies: ", this.regCheck, " and ", "RepeatedPassword:", this.regRepPassword);
   }
 
@@ -42,6 +58,7 @@ export class SignupComponent {
     // console.log(this.logPassword);
   }
 
+  // este es lo mismo, pero, para el reepetir contraseña
   assignPasswordValue2(event: any) {
     this.regRepPassword = event.target.value;
     // console.log(this.logPassword);
@@ -57,10 +74,34 @@ export class SignupComponent {
     // console.log("esta focused input contraseña")
   }
 
+  // lo mismo, pero, para el repetir contraseña
   onPasswordFocus2() {
     this.isPasswordFocused2 = true;
     // console.log("esta focused input contraseña")
   }
 
+
+  // Esta función sirve para verificar Si un texto tiene
+  //  espacios antes o despues de la primera secuencia 
+  //  continúa de letras o números y si los tiene Elimina 
+  //  estos espacios y, además, elimina todo el texto 
+  //  que haya después del segundo espacio en blanco Y se queda 
+  //  con el texto ( caracteres como letras o números ) que 
+  //  haya entre estos estos espacios
+  getTextBeforeFirstSpace(text: string): string {
+    // Eliminar espacios iniciales
+    text = text.trimStart();
+
+    // Buscar la posición del primer espacio después del texto
+    const firstSpaceIndex = text.indexOf(' ');
+
+    // Si no hay espacios, retornar el texto completo
+    if (firstSpaceIndex === -1) {
+      return text;
+    }
+
+    // Retornar el texto antes del primer espacio
+    return text.substring(0, firstSpaceIndex);
+  }
 
 }
