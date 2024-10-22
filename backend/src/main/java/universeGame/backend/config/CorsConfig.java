4bb893c7e4259712eec6cb9bcdad09fc9f6699df
miguel pimiento <mgefrad@gmail.com>
2024-10-22 09:11:51 -0500

    fix(cors origien): fix cors for jwt authentication

--- backend/src/main/java/universeGame/backend/config/CorsConfig.java
+++ backend/src/main/java/universeGame/backend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:4200", "http://20.55.104.54:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
