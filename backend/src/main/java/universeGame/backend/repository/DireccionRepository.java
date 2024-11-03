cf07a891788f75c66be727f2697efa40ce54189a
miguel pimiento <mgefrad@gmail.com>
2024-11-02 19:54:53 -0500

    feat(crud direccion) created crud direccion

--- backend/src/main/java/universeGame/backend/repository/DireccionRepository.java
+++ backend/src/main/java/universeGame/backend/repository/DireccionRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import universeGame.backend.model.Direccion;
 
+import java.util.Optional;
+
 @Repository
 public interface DireccionRepository extends JpaRepository<Direccion, Long> {
+
+    Optional<Direccion> findByUsuarioId(Long usuarioId);
+
+    boolean existsByUsuarioId(Long usuarioId);
 
 }
